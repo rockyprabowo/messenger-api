4b7833c5bdc29493f0c97adf11f27dd60efc31bf
Rocky Prabowo <rocky@lazycats.id>
2023-05-15 19:33:03 +0700

    wip: add raise and rescue from authorization failures in Authorization concern

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,12 +3,27 @@
 module Authorization
   extend ActiveSupport::Concern
 
+  class AccessForbidden < StandardError; end
+
   included do
     before_action :authorize
+
+    rescue_from Authorization::AccessForbidden, with: :four_zero_three
 
     def authorize
       auth = AuthorizeApiRequest.new(request.headers).call
       Current.user = auth[:user]
     end
+
+    def owner?(object_owner_id)
+      return true unless Current.user.id != object_owner_id
+
+      raise(Authorization::AccessForbidden, 'access denied')
+    end
   end
+
+  def four_zero_three(e)
+    json_response({ message: e.message }, :forbidden)
+  end
+
 end

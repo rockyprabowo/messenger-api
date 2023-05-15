d5c8a78abe749d6573a3d979db7461bad85a3e6b
Rocky Prabowo <rocky@lazycats.id>
2023-05-15 21:01:43 +0700

    wip: finish messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,37 +1,74 @@
 # frozen_string_literal: true
 
-class MessagesController
-  includes Authorization
+class MessagesController < ApplicationController
+  include Authorization
+  before_action :target_user_exists
+
   def create
-    if on_existing_conversation_with(params[:user_id])
-      new_message = ChatMessage.create(
-        conversation_id: @existing.conversation_id,
-        conversation_membership_id: @membership.id,
-        body: params[:message]
-      )
-      json_response new_message
+    if user_had_conversation_with @target_user
+      send_to_existing_conversation
+      json_response new_message_response, :created
+      return
     end
+    send_new_conversation_to @target_user
+    json_response new_message_response, :created
   end
 
   private
 
-  def on_existing_conversation_with(user_id)
-    @existing = ConversationMembership.pluck(:conversation_id)
-                                      .between([Current.user.id, user_id])
-                                      .first
+  def target_user_exists
+    @target_user = User.find(params[:user_id])
+  end
 
-    @membership = ConversationMembership.pluck(:id)
-                                        .of(@existing.conversation_id, Current.user.id)
-                                        .first
+  def user_had_conversation_with(user)
+    @existing_id = ConversationMembership
+                   .between_users(user_ids: [Current.user.id, user.id])
+                   .pluck(:conversation_id)
+                   .first
+
+    return false if @existing_id.blank?
+
+    obtain_user_membership
+    true
+  end
+
+  def obtain_user_membership
+    @membership_id = ConversationMembership
+                     .user_conversation(conversation_id: @existing_id, user_id: Current.user.id)
+                     .pluck(:id)
+                     .first
+  end
+
+  def send_to_existing_conversation
+    @new_message = send_message(
+      @existing_id, @membership_id, params[:message]
+    )
+  end
+
+  def send_new_conversation_to(user)
+    create_conversation_between(Current.user.id, user.id)
+    @new_message = send_message(@new_conversation.id, @new_membership.id, params[:message])
   end
 
   def create_conversation_between(creator_id, *user_ids)
     ActiveRecord::Base.transaction do
-      conversation = Conversation.create(user_id: creator_id)
-      ConversationMembership.create(conversation_id: conversation.id, user_id: creator_id)
-      ConversationMembership.create(user_ids.map do |id|
-        { conversation_id: conversation.id, user_id: id }
+      @new_conversation = Conversation.create!(user_id: creator_id)
+      @new_membership = ConversationMembership.create!(conversation_id: @new_conversation.id, user_id: creator_id)
+      ConversationMembership.create!(user_ids.map do |id|
+        { conversation_id: @new_conversation.id, user_id: id }
       end)
     end
+  end
+
+  def send_message(conversation_id, membership_id, body)
+    ChatMessage.create!(
+      conversation_id: conversation_id,
+      conversation_membership_id: membership_id,
+      body: body
+    )
+  end
+
+  def new_message_response
+    @new_message
   end
 end
